1568c6348e34fe310618a6c00050923085504d82
Korn <kornkubzaza@gmail.com>
2021-08-17 23:24:45 +0700

    อัพเดต

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "include/Game.h"
+#include "include/Room.h"
 
 using namespace std;
 
@@ -8,6 +9,9 @@ Game game(1080, 720);
 
 int main() {
 
+    Room room("rm_Game", 0, 1080, 720);
+
     game.run();
+
     return 0;
 }
