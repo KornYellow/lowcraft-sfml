2efa2d8c96eea9f0b8441c1c08ac4fc0aae2bef5
Korn <kornkubzaza@gmail.com>
2021-08-17 21:48:11 +0700

    เพิ่ม Room class

---
+++ include/Room.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+using namespace sf;
+
+class Room {
+
+    private:
+
+
+    public:
+    
+        Room(int window_width, int window_height);
+
+};
---
+++ source/Room.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+#include "include/Room.h"
+
+using namespace std;
+using namespace sf;
+
+Room::Room(int window_width, int window_height) {
+    
+}
