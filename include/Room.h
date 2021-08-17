0a09ce8d08e15b07bc43ddff1c6214ab2d994ee6
Korn <kornkubzaza@gmail.com>
2021-08-17 23:20:47 +0700

    เพิ่ม Method

--- include/Room.h
+++ include/Room.h
@@ -1,17 +1,31 @@
 #pragma once
 
+#include <iostream>
+
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
+#include "include/Game.h"
+
+using namespace std;
 using namespace sf;
 
 class Room {
 
     private:
 
+        Game* game;
 
-    public:
-    
-        Room(int window_width, int window_height);
+        int room_id;
+        string room_name;
+        int room_width;
+        int room_height;
+        int room_speed;
 
+    public: Room(string room_name, int room_id, int room_width, int room_height);
+
+        int getRoomID();
+        string getRoomName();
+        int getRoomWidth();
+        int getRoomHeight();
 };
--- source/Room.cpp
+++ source/Room.cpp
@@ -3,11 +3,38 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
+#include "include/Game.h"
 #include "include/Room.h"
 
 using namespace std;
 using namespace sf;
 
-Room::Room(int window_width, int window_height) {
-    
+Room::Room(string room_name, int room_id, int room_width, int room_height) {
+
+    this->room_name = room_name;
+    this->room_id = room_id;
+    this->room_width = room_width;
+    this->room_height = room_height;
+
+    cout << "Room " << room_name << " with an index of " << room_id << "." << endl;
+}
+
+int Room::getRoomID() {
+
+    return this->room_id;
+}
+
+string Room::getRoomName() {
+
+    return this->room_name;
+}
+
+int Room::getRoomWidth() {
+
+    return this->room_width;
+}   
+
+int Room::getRoomHeight() {
+
+    return this->room_height;
 }
