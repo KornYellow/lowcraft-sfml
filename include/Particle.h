ece24cfd9623ed7e8b863f075711cc976322f0d6
Korn <kornkubzaza@gmail.com>
2021-08-28 02:10:51 +0700

    Working on particle

---
+++ include/Particle.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+
+class Particle {
+
+    private :
+
+
+    public :
+
+        //Functions
+        void update();
+        void render();
+};  
